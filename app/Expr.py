cdad5448ca8402eb359fb33b21269298cfcb744e
Derek Fox <derekefox@gmail.com>
2024-11-25 16:49:52 -0500

    Finish GenerateAst.py to autogenerate Expr.py whenever we need to add new expression types.

--- app/Expr.py
+++ app/Expr.py
@@ -1,28 +1,43 @@
 from abc import ABC, abstractmethod
 from Token import Token
 
-class Expr(ABC): pass
+class Expr(ABC):
+	@abstractmethod
+	def accept(self, visitor: Visitor): pass
+class Visitor(ABC):
+	@abstractmethod
+	def visitBinaryExpr(self, expr: Binary): pass
+	@abstractmethod
+	def visitGroupingExpr(self, expr: Grouping): pass
+	@abstractmethod
+	def visitLiteralExpr(self, expr: Literal): pass
+	@abstractmethod
+	def visitUnaryExpr(self, expr: Unary): pass
 
 class Binary(Expr):
 	def __init__(self, left: Expr, op: Token, right: Expr, ):
 		self.left = left
 		self.op = op
 		self.right = right
-
+	def accept(self, visitor: Visitor):
+		visitor.visit(self)
 
 class Grouping(Expr):
 	def __init__(self, expr: Expr, ):
 		self.expr = expr
-
+	def accept(self, visitor: Visitor):
+		visitor.visit(self)
 
 class Literal(Expr):
 	def __init__(self, value: object, ):
 		self.value = value
-
+	def accept(self, visitor: Visitor):
+		visitor.visit(self)
 
 class Unary(Expr):
 	def __init__(self, op: Token, right: Expr, ):
 		self.op = op
 		self.right = right
-
+	def accept(self, visitor: Visitor):
+		visitor.visit(self)
 
--- tool/GenerateAst.py
+++ tool/GenerateAst.py
@@ -1,35 +1,46 @@
 import sys
 
-def defineType(file, base_name: str, class_name: str, fields: list):
-    file.write(f'class {class_name}({base_name}):\n')
 
-    file.write(f'\tdef __init__(self, ')
+def defineType(f, base_name: str, class_name: str, fields: list):
+    f.write(f'class {class_name}({base_name}):\n')
+
+    f.write(f'\tdef __init__(self, ')
     for name, type in fields:
-        file.write(f'{name}: {type}, ')
-    file.write('):\n')
+        f.write(f'{name}: {type}, ')
+    f.write('):\n')
 
     for name, _ in fields:
-        file.write(f'\t\tself.{name} = {name}\n')
-    file.write('\n\n')
+        f.write(f'\t\tself.{name} = {name}\n')
+
+    f.write(f'\tdef accept(self, visitor: Visitor):\n')
+    f.write(f'\t\tvisitor.visit(self)\n')
+    f.write('\n')
 
 
-def defineVisitor(file, base_name: str, types: dict):
-    file.write('class Visitor(ABC):\n')
+def defineVisitor(f, base_name: str, types: dict):
+    f.write('class Visitor(ABC):\n')
+    for class_name in types.keys():
+        f.write('\t@abstractmethod\n')
+        f.write(f'\tdef visit{class_name}{base_name}(self, expr: {class_name}): pass\n')
+    f.write('\n')
 
 
 def defineAST(output_dir: str, base_name: str, types: dict):
     path = f'{output_dir}/{base_name}.py'
 
-    with open(path, 'w') as file:
-        file.write('from abc import ABC, abstractmethod\n')
-        file.write('from Token import Token\n')
-        file.write('\n')
-        file.write(f'class {base_name}(ABC): pass\n\n')  # write base class
+    with open(path, 'w') as f:
+        f.write('from abc import ABC, abstractmethod\n')
+        f.write('from Token import Token\n')
+        f.write('\n')
+        f.write(f'class {base_name}(ABC):\n')
+        f.write('\t@abstractmethod\n')
+        f.write('\tdef accept(self, visitor: Visitor): pass\n')
 
-        defineVisitor(file, base_name, types)  # write visitors
+        defineVisitor(f, base_name, types)  # write visitors
 
         for class_name, fields in types.items():
-            defineType(file, base_name, class_name, fields)
+            defineType(f, base_name, class_name, fields)
+
 
 def main():
     if len(sys.argv) != 2:
