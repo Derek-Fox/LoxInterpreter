ff023591774bf487c199103eb15ecbf63fc6f972
Derek Fox <derekefox@gmail.com>
2024-12-10 09:26:29 -0500

    Add check_arg_types abstract method to LoxCallable.py

--- src/lox/LoxCallable.py
+++ src/lox/LoxCallable.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
 class LoxCallable(ABC):
+    name = "Base LoxCallable"
+
     @abstractmethod
     def arity(self) -> int:
         """Return the number of arguments the LoxCallable expects."""
@@ -10,3 +12,15 @@ class LoxCallable(ABC):
     def call(self, interpreter: "Interpreter", arguments: list[object]) -> object:
         """Call the LoxCallable with the given arguments. Uses the global environment of the passed interpreter."""
         pass
+
+    def check_arg_types(self, arg: str, *types: type[float | bool | str | list]):
+        type_map = {
+            float: "number",
+            bool: "boolean",
+            str: "string",
+            list: "list"
+        }
+
+        from lox.LoxRuntimeError import LoxRuntimeError
+        if not isinstance(arg, types):
+            raise LoxRuntimeError(message=f"Need arguments of type {[type_map[t] for t in types]} for {self.name}.")
